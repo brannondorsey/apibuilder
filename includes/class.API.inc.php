af7eaad082cceb308c2d7b3e6e351f747b8f51ba
Brannon Dorsey <brannon@brannondorsey.com>
2013-08-30 00:18:56 -0500

    First commit

---
+++ includes/class.API.inc.php
@@ -0,0 +1,325 @@
+<?php
+require_once("class.Database.inc.php");
+
+class API {
+
+	public $columns_to_provide;
+
+	protected $API_key_required = false;
+	protected $default_output_limit = 25;
+	protected $max_output_limit = 250;
+	protected $hits_per_day = 1000;
+	protected $default_order_by = "ORDER BY timestamp ";
+	protected $default_flow = "DESC ";
+	protected $search_in_boolean_mode = false; //used inside of form query for FULLTEXT searches
+	protected $search_has_been_repeated = false; //used to keep track if search has been repeated
+	protected $search = "";
+	protected $no_results_message = "no results found";
+	protected $full_text_columns;
+	protected $API_key;
+	
+	public function __construct(){
+		$this->columns_to_provide = 
+			"id, timestamp, filename, title, domain, url, referrer, ip, forward_from, author, owner, description, keywords, copywrite";
+		$this->full_text_columns = "url, description, keywords";
+	}
+
+	//Returns a valid JSON string from $_GET values. Array must be sanitized before using this function.
+	public function get_JSON_from_GET(&$get_array, $object_parent_name="data"){
+
+		$json_obj = new StdClass();
+
+		$query = $this->form_query($get_array);
+		if($this->check_API_key()){
+			//$this->JSON_string = $this->query_results_as_array_of_JSON_objs($query, $object_parent_name, true);
+
+			if($results_array = Database::get_all_results($query)){
+
+		 		if(is_array($results_array)){
+		 			// deletes key => value pairs if the value is empty. Only works if array is nested: 
+		 			// http://stackoverflow.com/questions/5750407/php-array-removing-empty-values	
+		 			$results_array = array_filter(array_map('array_filter', $results_array));
+		 			$json_obj->data = $results_array;
+		 			//COME BACK need to make count only parameter work
+		 		}
+
+		 	}else $json_obj->error = "no results found";
+
+			//only attempt to increment the api hit count if this method is called from a PUBLIC API request
+			if($this->API_key_required){
+				$query = "SELECT API_hit_date FROM " . Database::$table . " WHERE API_key = '" . $this->API_key . "' LIMIT 1";
+				$result = Database::get_all_results($query);
+				//increments the hit count and/or hit date OR sets the error message if the key has reached its hit limit for the day
+				if($this->update_API_hits($this->API_key, $result['API_hit_date']) === false){
+				 $json_obj->error = "API hit limit reached";
+		   		}
+			 }
+		}
+		else $json_obj->error = "API key is invalid or was not provided";
+		//if there was a search and it returned no results
+		if($this->search != "" &&
+			!$this->search_has_been_repeated &&
+			strstr($this->JSON_string, $this->no_results_message) != false){
+				$this->search_in_boolean_mode = true; //set search in boolean mode to true
+				$this->search_has_been_repeated = true; //note that the search will now have been repeated
+			 	//$this->JSON_string = $this->get_JSON_from_GET($get_array, $object_parent_name); //recurse the function (thus re-searching)
+			 	return $this->get_JSON_from_GET($get_array, $object_parent_name); //recurse the function (thus re-searching)
+			}
+		return json_encode($json_obj, JSON_PRETTY_PRINT);
+	}
+
+
+	// public function query_results_as_array_of_JSON_objs($query, $object_parent_name=NULL, $b_wrap_as_obj=false){
+	// 	$JSON_output_string = "";
+	// 	//if there were results output them as a JSON data obj
+	// 	//echo "the parent name is " . $object_parent_name . " and the boolean is " . $b_wrap_as_obj;
+	// 	if($results_array = Database::get_all_results($query)){
+	// 			//if the objects being output should be wrapped in an object specified by the parameters of this function
+	// 			if($object_parent_name != NULL && $b_wrap_as_obj){
+	// 			 	$JSON_output_string = '{"' . $object_parent_name . '":[';
+	// 			 }
+	// 			$JSON_output_string .= $this->output_objects($results_array);
+	// 			//see above
+	// 			if($object_parent_name != NULL && $b_wrap_as_obj){
+	// 				$JSON_output_string .= ']}';
+	// 			}
+	// 		}
+	// 	//if no results were found return a JSON error obj
+	// 	else $JSON_output_string = $this->get_error($this->no_results_message);
+	// 	return iconv('UTF-8', 'UTF-8//IGNORE', utf8_encode($JSON_output_string)); //make sure all database results are UTF-
+	// }
+
+	// //outputs JSON error object with error message argument
+	// public function get_error($error_message){
+	// 	return "{\"error\": \"$error_message\"}";
+	// }
+
+	//outputs JSON object from 1D or 2D MySQL results array
+	protected function output_objects($mysql_results_array){
+		$JSON_output_string = "";
+		$count_only_key = "count"; //change default COUNT(*) key name
+		if(isset($mysql_results_array[0])){
+			$i = 0;
+			foreach ($mysql_results_array as $user_row) {
+				$JSON_output_string .= "{";
+				foreach($user_row as $key => $value){
+					if($value != ""){
+						if($key == "COUNT(*)") $key = $count_only_key;
+						$JSON_output_string .= '"' . $key . '"' . ':';
+						$JSON_output_string .= '"' . $value . '"';
+						$JSON_output_string .= ',';
+					}
+				}
+				$JSON_output_string = trim($JSON_output_string, ",");
+				$JSON_output_string .= "}";
+				if ($i != sizeof($mysql_results_array) -1) $JSON_output_string .= ',';
+				$i++;
+			}
+		}
+		else{
+			$user_row = $mysql_results_array;
+			$JSON_output_string .= "{";
+				foreach($user_row as $key => $value){
+					if($value != ""){
+						if($key == "COUNT(*)") $key = $count_only_key;
+						$JSON_output_string .= '"' . $key . '"' . ':';
+						$JSON_output_string .= '"' . $value . '"';
+						$JSON_output_string .= ',';
+					}
+				}
+				$JSON_output_string = rtrim($JSON_output_string, ",");
+				$JSON_output_string .= "}";
+		}
+		return $JSON_output_string;
+	}
+
+	//builds a dynamic MySQL query statement from a $_GET array. Array must be sanitized before using this function.
+	protected function form_query(&$get_array){
+
+		$column_parameters = array();
+		$columns_to_provide_array = explode(', ', $this->columns_to_provide);
+		$this->search = "";
+		$order_by = "";
+		$flow = "";
+		$limit = "";
+		$page = 1;
+		$exact = false;
+		$count_only = false;
+		$exclude = array();
+		$this->API_key = "";
+
+		//distribute $_GETs to their appropriate arrays/vars
+		foreach($get_array as $parameter => $value){
+			if($this->is_column_parameter($parameter, $columns_to_provide_array)){ 
+				$column_parameters[$parameter] = $value;
+			}
+			else if($parameter == 'search') $this->search = $value;
+			else if($parameter =='order_by') $order_by = $value;
+			else if($parameter == 'flow') $flow = $value;
+			else if($parameter == 'limit') $limit = $value;
+			else if($parameter == 'page') $page = (int) $value;
+			else if($parameter == 'exact' &&
+				    strtolower($value) == "true") $exact = true;
+			else if($parameter == 'count_only' &&
+				    strtolower($value) == "true" ||
+				    $parameter == 'count_only' &&
+				    $value == true){
+				$count_only = true;
+			}
+			else if($parameter == 'exclude') $exclude = explode(",", $value);
+			else if($parameter == 'key') $this->API_key = $value; 
+		}
+
+		$match_against_statement = 'MATCH (' . $this->full_text_columns . ') AGAINST (\'' . $this->search . '\' IN BOOLEAN MODE) ';
+		if($count_only) $query = "SELECT COUNT(*)";
+		else $query = "SELECT " . $this->columns_to_provide;
+		if($this->search != ""){
+			//if the search is not supposed to be in boolean mode remove IN BOOLEAN MODE from $match_against_statment
+			if(!$this->search_in_boolean_mode) $match_against_statement = str_replace("IN BOOLEAN MODE", "", $match_against_statement);
+		 	$query .= ", " . $match_against_statement . "AS score";
+		}
+		$query .= " FROM "  . Database::$table ." ";
+
+		//if search was a parameter overide column paramters and use MATCH...AGAINST
+		if($this->search != ""){
+			$this->append_prepend($this->search, "'");
+			$query .= "WHERE $match_against_statement ORDER BY ";
+			//order by score if it is the first FULLTEXT (natural mode) search and order by likes if it is the fallback boolean mode search
+			$query .= ( $this->search_has_been_repeated ? "timestamp DESC " : "score DESC ");  
+		}
+		//if search was not specified as a parameter used use LIKE
+		else{
+			//add WHERE statements
+			if(sizeof($column_parameters) > 0){
+				$i = 0;
+				$query .= "WHERE ";
+				foreach ($column_parameters as $parameter => $value) {
+					//if exact parameter was specified as TRUE 
+					//or column parameter is id search by = not LIKE
+					if($parameter == 'id' || $exact){
+						$this->append_prepend($value, "'");
+					 	$query .= "$parameter = $value ";
+					}
+					else $query .= "$parameter LIKE '%$value%' ";
+					if($i != sizeof($column_parameters) -1) $query .= "AND ";
+					$i++;
+				}
+				//if there was an exclude parameter exclude each comma seperated value
+				//exclude cannot be used in a FULLTEXT search as of now
+				if(!empty($exclude)){
+					foreach($exclude as $excluded_id)
+					$query .= "AND id !='" . $excluded_id . "' ";
+
+				}
+			}
+		
+			//add ORDER BY statement
+			$order_by_string;
+			if($order_by != "" &&
+			$this->is_column_parameter($order_by, $columns_to_provide_array)){
+				$order_by_string = "ORDER BY $order_by ";
+			}
+			else $order_by_string = $this->default_order_by;
+			$query .= $order_by_string;
+
+			//add FLOW statement
+			$flow_string;
+			$flow = strtoupper($flow);
+			if($flow != "" &&
+			$flow == 'ASC' ||
+			$flow == 'DESC'){
+				$flow_string = "$flow ";
+			}
+			else $flow_string = $this->default_flow;
+			$query .= $flow_string;
+		}
+		//only add LIMIT of it is not a COUNT query
+		if(!$count_only){
+			//add LIMIT statement
+			$limit_string;
+			if($limit != ""){
+				$limit = (int) $limit;
+				if((int) $limit > $this->max_output_limit) $limit = $this->max_output_limit;
+				if((int) $limit < 1) $limit = 1;
+				$limit_string = "LIMIT $limit";
+			} 
+			else{
+				$limit = $this->default_output_limit;
+				$limit_string = "LIMIT $limit";	
+			} 
+			$query .= $limit_string;
+		}
+
+		//add PAGE statement
+		if($page != "" &&
+			$page > 1){
+			$query .= " OFFSET " . $limit * ($page -1);
+		}
+
+		//echo $query . "<br/>";
+		return $query;
+	}
+
+	protected function check_API_key(){
+		return true; //bypass API key
+		$API_key_query = "SELECT id FROM " . Database::$table . " WHERE API_key='" . $this->API_key ."' LIMIT 1";
+		//if the key was provided and it is the right length test it
+		if($this->API_key != "" &&
+			strlen($this->API_key) == 40){
+			$results = Database::get_all_results($API_key_query);
+			if($results &&
+				count($results) > 0){
+				//add insert SQL statement here to keep track of api hits 
+			 return true;
+			}
+			else return false;
+		}
+		//if the api key wasnt provided or isn't the right length return before querying
+		else return false;
+	}
+
+	//boolean that determines if API call limit has been reached
+	protected function call_limit_reached($API_key){
+		$query = "SELECT API_hits FROM " . Database::$table . " WHERE API_key='" . $API_key . "' LIMIT 1";
+		$results = Database::get_all_results($query);
+		$API_hits = $results['API_hits'];
+		return (intval($API_hits) >= $this->hits_per_day) ? true : false;
+	}
+
+	//need to put call_limit_reached inside of update_API_hits or something.
+	//fails to reset api calls when the call once the call limit has been reached.
+
+	//handles the incrementing of a key's API hits.
+	//returns false if user has maxed out hits for the day
+	public function update_API_hits($API_key, $API_hit_date){
+		//if the api has already been hit today
+		if(date('Ymd') == date('Ymd', strtotime($API_hit_date))){
+			//make sure that the key hasn't hit its limit.
+			//if it has return false
+			if(!$this->call_limit_reached($API_key)){
+				$query = "UPDATE " . Database::$table . " SET API_hits=API_hits+1 WHERE API_key='" . $API_key . "'";
+				Database::execute_sql($query);
+			}else return false;
+		}else{ //if the API has not been hit today set the hits to zero and the hit date to today
+			$now = new DateTime();
+			$query = "UPDATE " . Database::$table . " SET API_hits = 0, API_hit_date='" . $now->format(DateTime::ISO8601) . "' WHERE API_key='" . $API_key . "'";
+			Database::execute_sql($query);
+		}
+	}
+
+//------------------------------------------------------------------------------
+//HELPERS
+
+	//appends and prepends slashes to string for WHERE statement values
+	protected function append_prepend(&$string, $char){
+		$string = $char . $string . $char;
+	}
+
+	//checks if a parameter string is also the name of a SELECT statement's requested column
+	protected function is_column_parameter($parameter_name, $columns_to_provide_array){
+		return in_array ($parameter_name, $columns_to_provide_array);
+	}
+
+}
+
+?>
---
+++ includes/class.Database.inc.php
@@ -0,0 +1,118 @@
+<?php
+
+class Database {
+
+	public static $root_dir_link = "http://localhost:8888/api_builder";
+	public static $key = 12345;
+
+	//MySQL database info
+	protected static $user     = "root";
+	protected static $password = "root";
+	protected static $db       = "quartzite";
+  
+	public static $table    = "metadata";	
+	protected static $host  = "localhost";
+	protected static $mysqli;
+
+	//initialize the database connection
+	public static function init_connection(){
+		self::$mysqli = new mysqli(self::$host, self::$user, self::$password, self::$db);
+		return is_object(self::$mysqli);
+	}
+
+	//close the database connection
+	public static function close_connection(){
+		self::$mysqli->close();
+	}
+	
+	//execute sql query statement. Used for INSERT and UPDATE mostly. Returns false if query fails
+	public static function execute_sql($query) {
+		if(self::$mysqli->query($query)) return true;
+		else echo self::$mysqli->error;
+		return false;
+	}
+
+	//handles dynamic formation of INSERT and UPDATE queries from $_POST and executes them
+	//post array should be cleaned before using this function
+	public static function execute_from_assoc($post_array, $table_name, $statement_type="INSERT", $set_statement=NULL){
+		$statement_type = strtoupper($statement_type);
+		if($statement_type == "INSERT"){
+			$query = $statement_type . " INTO " . $table_name . " ("; 
+			foreach($post_array as $key => $value){
+				$query .= " `" . $key . "`,";
+			}
+			$query = rtrim($query, ",");
+			$query .= ") VALUES (";
+			foreach($post_array as $key => $value){
+				//if($key == 'lat' || $key == 'lon' || $value == 0) $query .= " " . $value . ",";
+				$query .= " '" . $value . "',";
+			}
+			$query = rtrim($query, ",");
+			$query .= ");";
+		}
+		//if statement type is UPDATE, the id of the row to update was specified in the $post_array,
+		//and what to update (set) was specified
+		else if($statement_type == "UPDATE" &&
+			    $set_statement != NULL){
+			$query = $statement_type . " " . $table_name . " SET " . $set_statement . " = '" . $post_array[$set_statement]
+			. "' LIMIT 1";
+		}
+		else{
+			echo "incorrect parameters passed to InsertUpdate::execute_from_assoc()";
+		 	return false;
+		}
+		return self::execute_sql($query);
+	}
+	
+	//returns array of one result row if one result was found or 2D array of all returned rows if multiple were found
+	public static function get_all_results($query) {
+		$result_to_return = array(); //maybe this shouldnt be like this...
+		if ($result = self::$mysqli->query($query)) {
+				$i=0;
+				while ($row = $result->fetch_assoc()) {
+					$result_to_return[$i] = $row;
+					$i++;	
+				}
+			if (count($result_to_return) > 1) {
+				return $result_to_return;
+			} 
+			else if(count($result_to_return) == 1) {
+				return $result_to_return[0];
+			} 
+			else return false; //there were no results found
+		}
+		else echo " MYSQL QUERY FAILED";
+	}
+
+	//returns string or assosciative array of strings
+	//mainly for $_POST and $_GET
+	public static function clean($string){
+		if(isset($string) && !empty($string)){
+			$new_string_array;
+			//if the string is actually an assoc array
+			if(is_array($string)){
+				foreach($string as $string_array_key => $string_array_value){
+					$string_array_value = self::clean_string($string_array_value);
+					$new_string_array[$string_array_key] = $string_array_value;
+				}
+				$string = $new_string_array;
+			}
+			//else just clean it
+			else $string = self::clean_string($string);
+			return $string;
+		}
+		else return false; //nothing valid was passed as an argument
+	}
+
+//------------------------------------------------------------------------------
+//HELPERS
+
+	//series of cleans to be perfomed on one string
+	protected static function clean_string($string){
+		$string = htmlspecialchars($string);
+		$string = self::$mysqli->real_escape_string($string);
+		return $string;
+	}
+}
+
+?>
